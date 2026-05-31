abc4143c6089ab936d56c6e8cff1869c2f701615
Narendran <narendrans1102@gmail.com>
2026-05-31 10:00:39 +0530

    fixed host for render deployment

--- app1.py
+++ app1.py
@@ -272,4 +272,5 @@ def analyze():
         return jsonify({"error": str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
